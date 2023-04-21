ad3a6e42834b9e8707858d04668f7f711b903f3c
VargasSantiago <vargas.santiago2002@gmail.com>
2023-04-21 17:57:50 -0300

    primer commit

---
+++ src/main/java/com/example/demo/controller/saludo.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class saludo {
+
+    @GetMapping("/saludo")
+    public ResponseEntity<?> saludar() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body("Hola Mundo");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" + e.getMessage() + "\"}"));
+        }
+    }
+}
